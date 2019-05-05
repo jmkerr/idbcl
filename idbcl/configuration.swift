ca489b68870d7aa4b2a8f871b07f0d73223085ae
Jonathan M Kerr <jonmkerr@outlook.com>
2019-05-05 16:01:11 +0200

    fix BAD_INSTR..

--- idbcl/configuration.swift
+++ idbcl/configuration.swift
@@ -7,6 +7,6 @@ import Foundation
 
 struct Configuration {
     static var executableURL: URL = URL(fileReferenceLiteralResourceName: CommandLine.arguments[0])
-    static var dbFileURL: URL? = CommandLine.argc == 2 ? URL(fileReferenceLiteralResourceName: CommandLine.arguments[1]) : nil
+    static var dbFileURL: URL? = CommandLine.argc == 2 ? URL(string: CommandLine.arguments[1]) : nil
     
 }
